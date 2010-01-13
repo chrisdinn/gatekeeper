2dffbdb236d8af3f7355b7032bbf17ad4b25d79d
Chris Dinn <chrisgdinn@gmail.com>
2010-01-12 19:18:20 -0500

    Simple refactor to make things simpler.

--- lib/keymaster/middleware.rb
+++ lib/keymaster/middleware.rb
@@ -1,6 +1,20 @@
 module Keymaster
   module Client
-    class Middleware < DefaultMiddleware
+    class Middleware < Sinatra::Base
+      enable :raise_errors
+      disable :show_exceptions
+
+      set :sso_url, nil
+      set :exclude_paths, nil
+
+      def sso_url=(url)
+        options.sso_url = url
+      end
+
+      def exclude_paths=(paths)
+        options.exclude_paths = paths
+      end
+      
       register ::Keymaster::Client::SSO
     end
   end
